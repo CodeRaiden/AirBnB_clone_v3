c845d59035db26eb04f3959d02eb76795c159467
CodeRaiden <hadoken10@yahoo.com>
2023-01-22 16:26:22 -0800

    add first endpoint (route) will be to return the status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+returns the status of the app_views object at route /status
+"""
+
+from . import app_views
+
+@app_views.route("/status")
+def status():
+    return jsonify(status="OK")
